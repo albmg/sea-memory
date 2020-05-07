6c1b1fa8712087bbcedddefa76d27b06148aa687
albmg <albmg@gmail.com>
2020-05-07 16:58:59 +0100

    game basic functionality

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,5 @@
 var animals = ["whale", "whale", "dolphin", "dolphin", "starfish", "starfish", "shark", "shark", "jellyfish",
-    "jellyfish", "squid", "squid", "octopus", "octopus", "fish", "fish", "sea turtle", "sea turtle", "crab", 
-    "crab", "kelp", "kelp", "penguin", "penguin", "seal", "seal", "krill", "krill"]
+    "jellyfish", "squid", "squid", "octopus", "octopus", "fish", "fish", "sea turtle", "sea turtle"]
 
 function shuffle(array) {
   var m = array.length, t, i;
@@ -14,17 +13,71 @@ function shuffle(array) {
     return array;
 }
 
-//console.log(shuffle(animals));
-//console.log(shuffle(animals[0]));
 
 var animalsShuffle = shuffle(animals);
 
+
 const cells = document.getElementsByClassName('cell');
-for (var i = 0; i < cells.length; i++) {
-    cells[i].innerText = animalsShuffle[i];
-    //console.log(cells[i]);
-    //console.log(animalsShuffle[i])
-    cells[i].onclick = function (e) {
-        console.log(e.target.innerText);
-    }
+
+function paintCards () {
+  //selectCell();  
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = animalsShuffle[i];       
+    cells[i].onclick = storeCell;    
+  }
 }
+
+var checkCards = [];
+
+function storeCell (e) {
+  //console.log(e);
+  switch (checkCards.length){
+    case 0: 
+      e.target.classList.add("active");
+      e.target.onclick = "";
+      checkCards.push(e.target.innerText);      
+      break;
+    case 1:
+      e.target.classList.add("active");
+      e.target.onclick = "";
+      checkCards.push(e.target.innerText);      
+      var checkingCard = document.getElementsByClassName("active");
+      for (var i = 0; i < checkingCard.length; i++) {
+        if (checkCards[0] === checkCards[1]) {     
+          checkingCard[i].classList.add("checked");
+          checkCards = [];
+          //console.log(checkCards.length);                    
+        } 
+        if (checkCards[0] !== checkCards[1] && !checkingCard[i].classList.contains("checked"))
+        {  
+          console.log(checkingCard[i]) 
+          checkingCard[i].classList.add("fail");                   
+        }
+        //if (checkCards[0] !== checkCards[1] && checkingCard[i].classList.contains("fail")) {
+        //else {  
+         // checkCards = [];
+        //}
+      }
+      console.log(checkCards.length);
+      break;
+    case 2:
+      checkCards = [];
+      var fails = document.querySelectorAll(".cell")
+      for (var i = 0; i < fails.length; i++) {
+        //console.log(fails[i]);
+        fails[i].classList.remove("fail");
+        fails[i].classList.remove("active");
+        fails[i].onclick = storeCell;
+      }
+      
+      cells.className = "";
+      break;
+    default:
+      //storeCell();
+  }
+  
+  
+  
+}
+
+paintCards();
