76f575ccaaeb1d581b3bace065131738dde9b04f
Alfonso C. Garcia Gimenez <agarcia@sirokko.es>
2020-05-09 14:21:04 +0100

    Fon changes

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,6 +1,81 @@
-// function selectNick
+import * as game from "./game.js";
+
+var ANIMALS = [
+  "whale",
+  "whale",
+  "dolphin",
+  "dolphin",
+  "starfish",
+  "starfish",
+  "shark",
+  "shark",
+  "seal",
+  "seal",
+  "squid",
+  "squid",
+  "octopus",
+  "octopus",
+  "crab",
+  "crab",
+  "seahorse",
+  "seahorse",
+];
+
+function Dom() {
+  var self = this;
+  this.animalsShuffle = [];
+
+  this.init = function () {
+    this.animalsShuffle = self.shuffle(ANIMALS);
+    this.layout();
+    let startButton = document.querySelector(".play-game");
+    let resetButton = document.querySelector(".restart-game");
+
+    startButton.addEventListener("click", game.gameInit);
+    resetButton.addEventListener("click", game.gameReset);
+  };
+
+  this.layout = function () {
+    let board = document.querySelector(".board");
+    for (let i = 0; i < 3 * 6; i++) {
+      const div = document.createElement("div");
+      div.classList.add("cell");
+      div.classList.add("aqua");
+      board.appendChild(div);
+    }
+
+    this.paintCards();
+  };
+
+  this.shuffle = function (items) {
+    var m = items.length,
+      t,
+      i;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = items[m];
+      items[m] = items[i];
+      items[i] = t;
+    }
+
+    return items;
+  };
+
+  this.paintCards = function () {
+    for (var i = 0; i < game.cells.length; i++) {
+      let animal = self.animalsShuffle[i];
+      game.cells[i].innerText = animal;
+      game.cells[i].classList.add(animal);
+      game.cells[i].onclick = game.checkCard;
+    }
+  };
+}
+
+var dom = new Dom();
+dom.init();
+
 var playerOneName = document.querySelectorAll(".player-one-name");
 var playerOneNick = document.querySelectorAll(".player-one");
 var playerTwoName = document.querySelectorAll(".player-two-name");
 var playerTwoNick = document.querySelectorAll(".player-two");
-    
--- scripts/game.js
+++ scripts/game.js
@@ -1,122 +1,113 @@
-var animals = ["whale", "whale", "dolphin", "dolphin", "starfish", "starfish", "shark", "shark", "seal",
-  "seal", "squid", "squid", "octopus", "octopus", "crab", "crab", "seahorse", "seahorse"]
+var players = ["p1", "p2"];
+var currentPlayer = players[0];
+export const cells = document.getElementsByClassName("cell");
 
-function shuffle(array) {
-  var m = array.length, t, i;
-    
-  while (m) {   
-    i = Math.floor(Math.random() * m--); 
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-    }
-    return array;
-}
-
-
-
-
-var animalsShuffle = shuffle(animals);
-
-
-const cells = document.getElementsByClassName('cell');
-
-function paintCards () {
-  //selectCell();  
-  for (var i = 0; i < cells.length; i++) {
-    let animal = animalsShuffle[i];
-    cells[i].innerText = animal;
-    cells[i].classList.add(animal);
-    cells[i].onclick = storeCell;    
+function selectPlayers() {
+  var player1 = document.querySelector(".player-one");
+  var p1 = player1.innerText;
+  console.log(p1);
+  console.log(player1);
+  if (players.indexOf("p1") !== -1) {
+    console.log(p1 + " es tu turno");
+    alert(`${p1} es tu turno`);
   }
 }
 
-function selectPlayers () {
-  var players = ["p1", "p2"];
-  var pl1 = document.querySelector(".player-one");
-  var p1 = pl1.innerText;
-    if (players.indexOf("p1")) {
-      console.log(p1 + " es tu turno");
-      alert(`${p1} es tu turno`);
-    }
-  
+function nextPlayer(current) {
+  let index =
+    players.indexOf(current) === -1 ? 0 : players.indexOf(current) + 1;
+  currentPlayer = players[index];
+  console.log(currentPlayer);
 }
 
-
 var checkCards = [];
+var checkedCards = [];
 
-function storeCell (e) {    
-  //console.log(e);
-  selectPlayers();
-  switch (checkCards.length){
-    case 0: 
+export function checkCard(event) {
+  const cards = Array.from(document.querySelectorAll(".cell"));
+
+  let actives = cards.filter(function (card) {
+    return card.classList.value.match(/active/);
+  });
+
+  let current = event.target;
+  current.classList.add("active");
+  current.onclick = null;
+  checkedCards.push(current);
+  let index = actives
+    .map((active) => active.innerText)
+    .indexOf(current.innerText);
+
+  if (index !== -1) {
+    checkedCards.forEach((c) => c.classList.add("checked"));
+  } else {
+    checkedCards.forEach((c) => c.classList.add("fail"));
+  }
+}
+
+export function storeCell(e) {
+  switch (checkCards.length) {
+    case 0:
       e.target.classList.add("active");
       e.target.onclick = "";
-      checkCards.push(e.target.innerText);      
+      checkCards.push(e.target.innerText);
       break;
     case 1:
       e.target.classList.add("active");
       e.target.onclick = "";
-      checkCards.push(e.target.innerText);      
+      checkCards.push(e.target.innerText);
       var checkingCard = document.getElementsByClassName("active");
+
       for (var i = 0; i < checkingCard.length; i++) {
-        if (checkCards[0] === checkCards[1]) {     
-          checkingCard[i].classList.add("checked");                   
+        if (checkCards[0] === checkCards[1]) {
+          checkingCard[i].classList.add("checked");
           checkCards = [];
-          //console.log(checkCards.length);                    
-        } 
-        if (checkCards[0] !== checkCards[1] && !checkingCard[i].classList.contains("checked"))
-        {  
-          console.log(checkingCard[i]) 
-          checkingCard[i].classList.add("fail");                   
-        }        
+          //console.log(checkCards.length);
+        }
+        if (
+          checkCards[0] !== checkCards[1] &&
+          !checkingCard[i].classList.contains("checked")
+        ) {
+          console.log(checkingCard[i]);
+          checkingCard[i].classList.add("fail");
+        }
       }
-      console.log(checkCards.length);
+      console.log("length of checkcards", checkCards.length);
       break;
     case 2:
       checkCards = [];
-      var fails = document.querySelectorAll(".cell")
-      for (var i = 0; i < fails.length; i++) {        
+      var fails = document.querySelectorAll(".cell");
+
+      for (var i = 0; i < fails.length; i++) {
         fails[i].classList.remove("fail");
         fails[i].classList.remove("active");
         fails[i].onclick = storeCell;
       }
-      
+
+      nextPlayer(currentPlayer);
+
       cells.className = "";
       break;
     default:
-      //storeCell();
-  }  
+    //storeCell();
+  }
 }
+
 function selectNick() {
-  document.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
       // code for enter
-      playerOneNick[0].innerText = playerOneName[0].value
-      playerTwoNick[0].innerText = playerTwoName[0].value
+      playerOneNick[0].innerText = playerOneName[0].value;
+      playerTwoNick[0].innerText = playerTwoName[0].value;
     }
   });
 }
 
-function gameInit (){
-  selectNick();    
-  // function gameInit
-  /*var board = document.querySelector(".board");
-  var div = document.createElement("div");
-  div.classList.add("cell");
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 6; j++) {
-      board.append(div)
-    }
-  }*/
-  var playGame = document.querySelectorAll(".play-game")
-  playGame[0].onclick = paintCards;
-  var resetGame = document.querySelectorAll(".restart-game");
-  resetGame[0].addEventListener("click", gameReset);
+export function gameInit() {
+  selectNick();
 }
 
-function gameReset () {
+export function gameReset() {
   window.location.reload(true);
 }
 
