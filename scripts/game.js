a968f520438d890e439dd6598b34c05a055f15c3
Alfonso C. Garcia Gimenez <agarcia@sirokko.es>
2020-05-15 19:37:11 +0100

    trying to game properly

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,55 @@
 function Player(name, letter) {
   this.name = document.querySelector(name).value;
-  this.classString = `players-${letter}`
+  this.classString = `players-${letter}`;
 }
 
 function Game() {
-  const ANIMALS = [ "whale", "whale", "dolphin", "dolphin", "starfish", "starfish", "shark", "shark", "seal", "seal", "squid", "squid", "octopus", "octopus", "crab", "crab", "seahorse", "seahorse"];
+  const ANIMALS = [
+    "whale",
+    "whale",
+    "dolphin",
+    "dolphin",
+    "starfish",
+    "starfish",
+    "shark",
+    "shark",
+    "seal",
+    "seal",
+    "squid",
+    "squid",
+    "octopus",
+    "octopus",
+    "crab",
+    "crab",
+    "seahorse",
+    "seahorse",
+  ];
+
+  this.selectedCards = [];
 
   this.init = function () {
     this.animals = this.shuffle(ANIMALS);
     let board = document.querySelector(".board");
 
-    this.player1 = new Player(".player-one-name", 'a');
-    this.player2 = new Player(".player-two-name", 'b');
+    this.player1 = new Player(".player-one-name", "a");
+    this.player2 = new Player(".player-two-name", "b");
     this.currentPlayer = this.player1;
-    document.querySelector('.players-a').classList.add('hit')
-    this.animals.forEach(animal => {
+    document.querySelector(".players-a").classList.add("hit");
+    this.animals.forEach((animal) => {
       const div = document.createElement("div");
       div.classList.add("cell");
       div.innerText = animal;
       div.classList.add(animal);
-      div.classList.add('cell-hidden');
-      div.addEventListener('click', this.play);
+      div.classList.add("cell-hidden");
+      div.addEventListener("click", this.play);
       board.appendChild(div);
-    })
+    });
   };
 
   this.shuffle = function (array) {
-    var m = array.length, t, i;
+    var m = array.length,
+      t,
+      i;
 
     while (m) {
       i = Math.floor(Math.random() * m--);
@@ -37,21 +60,21 @@ function Game() {
     return array;
   };
 
-  this.nextPlayer = function() {
+  this.nextPlayer = function () {
     if (this.currentPlayer === this.player1) {
-      this.currentPlayer = this.player2
+      this.currentPlayer = this.player2;
       document.querySelector(".players-b").classList.add("hit");
       document.querySelector(".players-a").classList.remove("hit");
     } else {
-      this.currentPlayer = this.player1
+      this.currentPlayer = this.player1;
       document.querySelector(".players-a").classList.add("hit");
       document.querySelector(".players-b").classList.remove("hit");
     }
-  }
+  };
 
-  this.starTimer = function() {
+  this.starTimer = function () {
     this.initialTime = 20;
-    this.timerId = setInterval( () => {
+    this.timerId = setInterval(() => {
       document.getElementById("timer").value = this.initialTime;
       if (this.initialTime === 0) {
         clearInterval(this.timerId);
@@ -61,40 +84,61 @@ function Game() {
     }, 1000);
 
     return this.timerId;
-  }
+  };
 
-  this.stopTimer = function() {
-    clearInterval(this.timerId)
-  }
+  this.stopTimer = function () {
+    clearInterval(this.timerId);
+  };
 
-  this.play = e => {
-    let card = e.target
-    card.classList.add("clicked");    
-    card.onclick = "";    
-    // this.starTimer()
-    var selectedCards = document.getElementsByClassName("clicked");       
-    if (selectedCards.length === 2) {
-      if (selectedCards[0].innerText === selectedCards[1].innerText) {        
-        selectedCards[0].classList.add('hit')
-        selectedCards[0].classList.add(this.currentPlayer.classString)
-        selectedCards[1].classList.add('hit')
-        selectedCards[1].classList.add(this.currentPlayer.classString)
+  this.play = (e) => {
+    let card = e.target;
+    //card.classList.toggle("cell-hidden");
+    card.classList.remove("cell-hidden");
+    // card.classList.add("clicked");
+    this.selectedCards.push(card);
 
-        selectedCards[0].classList.remove('cell-hidden')
-        selectedCards[1].classList.remove('cell-hidden')
-        selectedCards[0].classList.remove('clicked')
-        selectedCards[0].classList.remove('clicked')
-      } else {  
-        console.log("diferentes");         
-        selectedCards[0].classList.remove('clicked')
-        selectedCards[0].classList.remove('clicked')
-        this.nextPlayer()
+    t = setTimeout(() => {
+      card.classList.add("cell-hidden");
+    }, 2000);
+
+    card.onclick = "";
+    //this.starTimer()
+    // var selectedCards = document.getElementsByClassName("clicked");
+
+    console.log("Selected cards", this.selectedCards);
+
+    //console.log(selectedCards.length)
+    if (this.selectedCards.length === 2) {
+      if (this.selectedCards[0].innerText === this.selectedCards[1].innerText) {
+        //clearTimeout(t);
+        this.selectedCards[0].classList.add("hit");
+        this.selectedCards[0].classList.add(this.currentPlayer.classString);
+        //clearTimeout(t);
+        this.selectedCards[1].classList.add("hit");
+        this.selectedCards[1].classList.add(this.currentPlayer.classString);
+        //clearTimeout(t);
+
+        this.selectedCards[0].classList.remove("cell-hidden");
+        this.selectedCards[1].classList.remove("cell-hidden");
+        // selectedCards[0].classList.remove("clicked");
+        // selectedCards[0].classList.remove("clicked");
+        console.log("Selected cards", this.selectedCards);
+
+        this.selectedCards = [];
+
+        clearTimeout(t);
+      } else {
+        // selectedCards[0].classList.remove("clicked");
+        // selectedCards[1].classList.remove("clicked");
+        console.log("Selected cards", this.selectedCards);
+
+        this.selectedCards = [];
+
+        this.nextPlayer();
       }
-    }    
-  }
+    }
+  };
 }
-
-
 
 /*var checkCards = [];
 
@@ -149,50 +193,48 @@ function storeCards (e) {
   }
 }*/
 
-
-
 var playerOneName = document.querySelectorAll(".player-one-name");
 var playerTwoName = document.querySelectorAll(".player-two-name");
 
 function selectNick() {
-  playerOneName[0].addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
+  playerOneName[0].addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
       playerOneNick[0].innerText = playerOneName[0].value;
     }
-  })
-  playerTwoName[0].addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
+  });
+  playerTwoName[0].addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
       playerTwoNick[0].innerText = playerTwoName[0].value;
     }
-  })
+  });
 }
 
 /*function blockNick () {
   playerOneName[0].removeEventListener("disabled", EventTarget);
 }*/
 
-function changeBackground () {
+function changeBackground() {
   var backg = document.getElementsByClassName("board");
   var h1text = document.getElementsByTagName("h1");
-  backg[0].style.backgroundColor = "white";
+  backg[0].style.background = "none";
   //h1text[0].innerText = "";
 }
 
-function gameReset () {
+function gameReset() {
   window.location.reload(true);
 }
 
-
 document.querySelector(".play-game").addEventListener("click", startGame);
 /*playGame.removeEventListener("click", iniciarPartida);*/
-document.querySelector(".restart-game").addEventListener("click", this.gameReset);
-
+document
+  .querySelector(".restart-game")
+  .addEventListener("click", this.gameReset);
 
 selectNick();
 
-function startGame () {
+function startGame() {
   var game = new Game();
-  document.querySelector('.start-panel').style.display = 'none'
+  document.querySelector(".start-panel").style.display = "none";
   game.init();
   changeBackground();
   //blockNick();
